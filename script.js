f6943dfaa286f5e22e0247a50f3ddde2f14a8ceb
emmagicarpe <emmafoulon26@gmail.com>
2026-04-17 15:28:59 +0200

    feat: initial project structure with landing page

---
+++ script.js
@@ -0,0 +1,10 @@
+/* EVENT LISTENERS */
+// Start the quiz when the user clicks the start button
+const startBtn = document.getElementById('start-btn');
+startBtn.addEventListener('click', () => startQuiz());
+
+/* FUNCTIONS */
+function startQuiz()
+{
+    console.log("Quiz started!");
+}
